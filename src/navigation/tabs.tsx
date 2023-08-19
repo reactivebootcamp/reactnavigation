667729ac6790801873f8888a131a2c68a1fd351a
Daniel Soto <dasg.1993@gmail.com>
2023-08-19 12:45:58 -0500

    [feature] - create custom buttom tab navigator

--- src/navigation/tabs.tsx
+++ src/navigation/tabs.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 import {ShopStack} from './shop';
-import {Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import DrawerNavigator from './drawer';
 
 const homeIcon = require('../../assets/images/home.png');
@@ -19,33 +22,100 @@ const styles = StyleSheet.create({
   },
 });
 
+const TabBar = ({
+  state,
+  descriptors,
+  navigation,
+  insets,
+}: BottomTabBarProps) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        paddingBottom: insets.bottom,
+        backgroundColor: '#EBEBD3',
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+      }}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingVertical: 20,
+            }}>
+            <Text style={{color: isFocused ? '#673ab7' : '#222'}}>{label}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
 const TabsNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName="MainStack"
+      tabBar={props => <TabBar {...props} />}
       screenOptions={{
         headerShown: false,
-        tabBarLabelStyle: {
-          fontWeight: '400',
-          fontSize: 11,
-        },
-        tabBarStyle: {
-          backgroundColor: '#fff',
-        },
-        tabBarActiveTintColor: '#EC5B70',
-        tabBarInactiveTintColor: '#212121',
+        // tabBarLabelStyle: {
+        //   fontWeight: '400',
+        //   fontSize: 11,
+        // },
+        // tabBarStyle: {
+        //   backgroundColor: '#fff',
+        // },
+        // tabBarActiveTintColor: '#EC5B70',
+        // tabBarInactiveTintColor: '#212121',
       }}>
       <Tab.Screen
         name="DrawerStack"
         component={DrawerNavigator}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({focused}) => (
-            <Image
-              style={styles.icon}
-              source={focused ? homeIcon : homeIconOutline}
-            />
-          ),
+          // tabBarIcon: ({focused}) => (
+          //   <Image
+          //     style={styles.icon}
+          //     source={focused ? homeIcon : homeIconOutline}
+          //   />
+          // ),
         }}
       />
       <Tab.Screen
@@ -53,12 +123,12 @@ const TabsNavigator = () => {
         component={ShopStack}
         options={{
           tabBarLabel: 'Shop',
-          tabBarIcon: ({focused}) => (
-            <Image
-              style={styles.icon}
-              source={focused ? shopIcon : shopIconOutline}
-            />
-          ),
+          // tabBarIcon: ({focused}) => (
+          //   <Image
+          //     style={styles.icon}
+          //     source={focused ? shopIcon : shopIconOutline}
+          //   />
+          // ),
         }}
       />
     </Tab.Navigator>
