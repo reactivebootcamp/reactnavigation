2e041eb1133c64ceaa042400a02452168eda318e
Daniel Soto <dasg.1993@gmail.com>
2023-08-28 13:52:54 -0500

    [feature] - fix selector

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import TabsNavigator from './tabs';
 import {AuthStack} from './auth';
-import {useAppSelector} from '../store';
+import {useSelector} from '../store';
 // import {useAuth} from '../features/auth/context';
 
 export const RootNavigator = () => {
-  const auth = useAppSelector(state => state.auth);
+  const auth = useSelector(state => state.auth);
   const {isAuthenticated} = auth;
 
   console.warn({auth});
